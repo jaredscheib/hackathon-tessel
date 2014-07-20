c5c49ed3adcf56b3195a059cc2a81d1e042c2430
jaredscheib <jared.scheib@gmail.com>
2014-07-19 19:17:41 -0700

    Final attempt, not fully functional because of error in image from fs.readFileSync / process.sendfile

--- tessel-modules/camera.js
+++ tessel-modules/camera.js
@@ -4,11 +4,22 @@ var camera = require( 'camera-vc0706' ).use( tessel.port['D'] );
 var net = require('net');
 var config = require('../config.js');
 
-process.env.TESSEL_UPLOAD_DIR = './images';
+var fs = require('fs');
 
-var cameraConnection = net.connect(config.port, config.host, function(){
-  console.log('Camera connected to server');
-});
+process.env.TESSEL_UPLOAD_DIR = './images';
+var cameraConnection;
+
+var cameraConnectionInit = function() {
+  cameraConnection = net.connect(config.port, config.host, function(){
+    console.log('Camera connected to server');
+  });
+
+  cameraConnection.on('data', function(data) {
+    data = JSON.parse(data.toString());
+    console.log('data: ', data);
+    cameraRouter[data.command].call(null, data.param);
+  });
+};
 
 var notificationLED = tessel.led[3]; //LED to notify when we're taking a picture
 
@@ -18,37 +29,45 @@ var cameraInit = function() {
 
   camera.on( 'ready', function() {
     notificationLED.high();
-    // camera.setCompression( 0 ); //set compression, minimum is ?
-    camera.isReady = true;
-    console.log('camera ready');
+    camera.setResolution( 'vga', function(){
+      // camera.setCompression( 0 ); //set compression, minimum is ?
+      camera.isReady = true;
+    }); //set picture resolution, highest is vga
+  });
 
-    cameraConnection.on('data', function(data) {
-      console.log(data);
-      data = JSON.parse(data.toString());
-      console.log('data: ', data);
-      cameraRouter[data.command].call(null, data.param);
-    });
+  camera.on ('error', function( err ) {
+    console.error( err );
   });
 };
 
-camera.on ('error', function( err ) {
-  console.error( err );
-});
-
+//various commands that client might receive from server
 cameraRouter = {
-  capturePhoto: function(frames) {
+  cameraCapture: function(frames) {
     return stream(frames);
+  },
+  cameraStart: function() {
+    cameraInit();
+    console.log('camera initialized');
+  },
+  cameraStop: function() {
+    camera.disable();
+    console.log('camera disabled');
+  },
+  cameraConnect: function() {
+    cameraConnectionInit();
+  },
+  cameraDisconnect: function(callback) {
+    cameraConnection.end(function(){
+      callback();
+    });
   }
 };
 
 var stream = function( frames ) {
   if( !camera.isReady ) return;
 
-  camera.setResolution( 'vga' ); //set picture resolution, highest is vga
-  if( frames <= 0 ) {
-    console.log('disable camera');
-    return camera.disable();
-  }
+  if( frames === undefined ) frames = 1;
+  if( frames <= 0 ) return console.log( 'all photos captured');
   frames = frames || 1;
   console.log( frames, 'photos remaining. capturing photo...' );
 
@@ -57,21 +76,29 @@ var stream = function( frames ) {
       console.log( 'error taking image', err );
     }else{
       notificationLED.low();
-      // console.log('hello');
-      // var name = 'VID_' + Math.floor(Date.now()*1000) + '.jpg';
-      // console.log( 'Saving frame as', name, '...' );
-      // process.sendfile( name, image );
+      var name = 'IMG_' + Math.floor(Date.now()*1000) + '.jpg';
+      console.log( 'Saving frame as', name, '...' );
+      console.log(_dirname);
+      fs.writeFileSync( __dirname + '/images/' + name, image );
+      console.log('file done');
+      cameraRouter.cameraStop();
       console.log('sending photo buffer to server: ', image)
-      cameraConnection.write(image);
-      console.log( 'photo sent to server.' );
+      cameraConnection.write(image, function(){
+        console.log( 'photo sent to server.' );
+        cameraRouter.cameraDisconnect(function(){
+          console.log('camera disconnected from server');
+          cameraRouter.cameraConnect();
+        });
+      });
     }
     frames--;
     return stream( frames );
   });
 };
 
-cameraInit();
+cameraRouter.cameraStart();
+cameraRouter.cameraConnect();
 
 module.exports = {
   stream: stream
-}
+};
